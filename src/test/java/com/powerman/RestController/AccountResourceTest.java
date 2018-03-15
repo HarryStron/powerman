1e07c6ccbca3ef3f66bc8776612b43584a117b14
Harry Strongyloudis <charalampos.strongyloudis@openmarket.com>
2018-03-15 14:48:36 +0000

    meh

--- src/test/java/com/powerman/RestController/AccountResourceTest.java
+++ src/test/java/com/powerman/RestController/AccountResourceTest.java
@@ -1,6 +1,6 @@
 package com.powerman.RestController;
 
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.ws.rs.core.Response;
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,12 +31,11 @@ public class AccountResourceTest {
 
     @Test
     public void canPOST() throws Exception {
-        given(accountResourceMock.createAccount(123)).willReturn(Response.created(UriBuilder.fromPath("/123").build()).build());
+        Response.ResponseBuilder created = Response.created(UriBuilder.fromPath("/123").build());
+        when(accountResourceMock.createAccount(123)).thenReturn(created.build());
 
         mockMvc.perform(MockMvcRequestBuilders
                 .request(HttpMethod.POST, "/account")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
                 .content("{\t\"accountId\": \"123\"}"))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
     }
